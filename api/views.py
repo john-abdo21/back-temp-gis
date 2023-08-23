27e9dcbc1b9156b61abb4399cf024891de6c4693
root <root@vmi1336980.contaboserver.net>
2023-08-23 06:52:08 +0200

    finish with land, river, lake, forest

--- api/views.py
+++ api/views.py
@@ -29,6 +29,8 @@ def complex_Search(request):
         return Response(status=status.HTTP_200_OK)
     elif request.method == 'POST':
         print('complex search',request.data)
+
+        # Importing variables
         data = request.data.get('data')
 
         land_option = data['landOption']
@@ -37,30 +39,320 @@ def complex_Search(request):
         l_min_aed = land_option['l_min_aed']
         l_max_aed = land_option['l_max_aed']
 
-        if l_min_area != '' or l_max_area != '':
+        options = data['options']
+
+        f_min_distance = options['forest']['minDistance']
+        f_max_distance = options['forest']['maxDistance']
+        f_min_area = options['forest']['minArea']
+        f_max_area = options['forest']['maxArea']
+
+        r_min_distance = options['river']['minDistance']
+        r_max_distance = options['river']['maxDistance']
+        r_min_length = options['river']['minLength']
+        r_max_length = options['river']['maxLength']
+        r_min_width = options['river']['minWidth']
+        r_max_width = options['river']['maxWidth']
+
+        i_min_distance = options['lake']['minDistance']
+        i_max_distance = options['lake']['maxDistance']
+        i_min_area = options['lake']['minArea']
+        i_max_area = options['lake']['maxArea']
+
+        t_min_distance = options['town']['minDistance']
+        t_max_distance = options['town']['maxDistance']
+        t_min_area = options['town']['minArea']
+        t_max_area = options['town']['maxArea']
+        t_hospital_exist = options['town']['hospital']
+
+
+        # Transforming the data unit
+        if l_min_area:
+            l_min_area = float(l_min_area) * 10000
+        if l_max_area:
+            l_max_area = float(l_max_area) * 10000
+
+        if r_min_distance:
+            r_min_distance = float(r_min_distance)
+        if r_max_distance:
+            r_max_distance = float(r_max_distance)
+        if r_min_length:
+            r_min_length = float(r_min_length)
+        if r_max_length:
+            r_max_length = float(r_max_length)
+        if r_min_width:
+            r_min_width = float(r_min_width)
+        if r_max_width:
+            r_max_width = float(r_max_width)
+
+        # SQL query generates
+        sqlForRegions = ''''''
+        if l_min_area or l_max_area:
             if l_min_area == '':
                 sqlForRegions = '''
-                    SELECT ST_AsGeoJSON(ST_Union(ST_Transform(geom, 3857))) AS merged_geojson FROM pl_plot3857 WHERE area <= %(l_max_area)s;
+                    SELECT
+                        ST_AsGeoJSON(ST_Union(ST_Transform(geom, 3857)))
+                    AS
+                        merged_geojson
+                    FROM
+                        pl_plot3857
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(geom, 3857)), 0)
+                        AS in_distance
+                        FROM pl_plot3857
+                        WHERE area <= %(l_max_area)s
+                    ) AS pl_plot3857_in_distance
+                    ON ST_Intersects(pl_plot3857.geom, pl_plot3857_in_distance.in_distance)
                 '''
-                with connection.cursor() as cursor:
-                    cursor.execute(sqlForRegions,{'l_max_area': l_max_area})
-                    overlapping_regions = cursor.fetchall()
-            if l_max_area == '':
+            
+            elif l_max_area == '':
                 sqlForRegions = '''
-                    SELECT ST_AsGeoJSON(ST_Union(ST_Transform(geom, 3857))) AS merged_geojson FROM pl_plot3857 WHERE area >= %(l_min_area)s;
+                    SELECT
+                        ST_AsGeoJSON(ST_Union(ST_Transform(geom, 3857)))
+                    AS
+                        merged_geojson
+                    FROM
+                        pl_plot3857
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(geom, 3857)), 0)
+                        AS in_distance
+                        FROM pl_plot3857
+                        WHERE area >= %(l_min_area)s
+                    ) AS pl_plot3857_in_distance
+                    ON ST_Intersects(pl_plot3857.geom, pl_plot3857_in_distance.in_distance)
                 '''
-                with connection.cursor() as cursor:
-                    cursor.execute(sqlForRegions,{'l_min_area': l_min_area})
-                    overlapping_regions = cursor.fetchall()
-            else:
-                if l_min_area <= l_max_area:
-                    sqlForRegions = '''
-                        SELECT ST_AsGeoJSON(ST_Union(ST_Transform(geom, 3857))) AS merged_geojson FROM pl_plot3857 WHERE area >= %(l_min_area)s AND area <= %(l_max_area)s;
-                    '''
-                    with connection.cursor() as cursor:
-                        cursor.execute(sqlForRegions,{'l_max_area': l_max_area, 'l_min_area': l_min_area})
-                        overlapping_regions = cursor.fetchall()
+
+            elif l_min_area <= l_max_area:
+                sqlForRegions = '''
+                    SELECT
+                        ST_AsGeoJSON(ST_Union(ST_Transform(geom, 3857)))
+                    AS
+                        merged_geojson
+                    FROM
+                        pl_plot3857
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(geom, 3857)), 0)
+                        AS in_distance
+                        FROM pl_plot3857
+                        WHERE area >= %(l_min_area)s AND area <= %(l_max_area)s
+                    ) AS pl_plot3857_in_distance
+                    ON ST_Intersects(pl_plot3857.geom, pl_plot3857_in_distance.in_distance)
+                '''
+
+        else:
+            return True
         
+        r_where_SQL = ''''''
+        if r_min_length or r_max_length:
+            if r_min_length == '':
+                r_where_SQL = '''
+                    WHERE dlug <= %(r_max_length)s
+                '''
+            elif r_max_length == '':
+                r_where_SQL = '''
+                    WHERE dlug >= %(r_min_length)s
+                '''
+            elif r_min_length <= r_max_length:
+                r_where_SQL = '''
+                    WHERE dlug >= %(r_min_length)s AND dlug <= %(r_max_length)s
+                '''
+
+        if r_min_width or r_max_width:
+            if r_where_SQL:
+                if r_min_width == '':
+                    r_where_SQL += '''
+                        AND r_width <= %(r_max_width)s
+                    '''
+                elif r_max_width == '':
+                    r_where_SQL += '''
+                        AND r_width >= %(r_min_width)s
+                    '''
+                elif r_min_width <= r_max_width:
+                    r_where_SQL += '''
+                        AND r_width >= %(r_min_width)s AND r_width <= %(r_max_width)s
+                    '''
+            else:
+                if r_min_width == '':
+                    r_where_SQL = '''
+                        WHERE r_width <= %(r_max_width)s
+                    '''
+                elif r_max_width == '':
+                    r_where_SQL = '''
+                        WHERE r_width >= %(r_min_width)s
+                    '''
+                elif r_min_width <= r_max_width:
+                    r_where_SQL = '''
+                        WHERE r_width >= %(r_min_width)s AND r_width <= %(r_max_width)s
+                    '''
+
+        if r_min_distance or r_max_distance:
+            if r_min_distance == '':
+                sqlForRegions += '''
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(geom, 3857)), %(r_max_distance)s) AS buffer_geom
+                        FROM pl_river3857
+                    '''
+                sqlForRegions += r_where_SQL
+                sqlForRegions += '''
+                    )
+                '''
+            elif r_max_distance == '':
+                r_max_distance = r_min_distance + 100000000
+                sqlForRegions += '''
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(geom, 3857)), %(r_max_distance)s) AS buffer_geom
+                        FROM pl_river3857
+                    '''
+                sqlForRegions += r_where_SQL
+                sqlForRegions += '''
+                    )
+                '''
+            elif r_min_distance <= r_max_distance:
+                sqlForRegions += '''
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(geom, 3857)), %(r_max_distance)s) AS buffer_geom
+                        FROM pl_river3857
+                    '''
+                sqlForRegions += r_where_SQL
+                sqlForRegions += '''
+                    )
+                '''
+            sqlForRegions += '''
+                AS pl_river3857_buffer
+                ON ST_Intersects(pl_plot3857.geom, pl_river3857_buffer.buffer_geom)
+            '''
+        
+        i_where_SQL = ''''''
+        if i_min_area or i_max_area:
+            if i_min_area == '':
+                i_where_SQL = '''
+                    WHERE area <= %(i_max_area)s
+                '''
+            elif i_max_area == '':
+                i_where_SQL = '''
+                    WHERE area >= %(i_min_area)s
+                '''
+            elif i_min_area <= i_max_area:
+                i_where_SQL = '''
+                    WHERE area >= %(i_min_area)s AND area <= %(i_max_area)s
+                '''
+
+        if i_min_distance or i_max_distance:
+            if i_min_distance == '':
+                sqlForRegions += '''
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(shape, 3857)), %(i_max_distance)s) AS inlandwater_buffer_shape
+                        FROM inlandwater
+                    '''
+                sqlForRegions += i_where_SQL
+                sqlForRegions += '''
+                    )
+                '''
+            elif i_max_distance == '':
+                i_max_distance = i_min_distance + 100000000
+                sqlForRegions += '''
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(shape, 3857)), %(i_max_distance)s) AS inlandwater_buffer_shape
+                        FROM inlandwater
+                    '''
+                sqlForRegions += i_where_SQL
+                sqlForRegions += '''
+                    )
+                '''
+            elif i_min_distance <= i_max_distance:
+                sqlForRegions += '''
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(shape, 3857)), %(i_max_distance)s) AS inlandwater_buffer_shape
+                        FROM inlandwater
+                    '''
+                sqlForRegions += i_where_SQL
+                sqlForRegions += '''
+                    )
+                '''
+            sqlForRegions += '''
+                AS inlandwater_buffer
+                ON ST_Intersects(pl_plot3857.shape, inlandwater_buffer.inlandwater_buffer_shape)
+            '''
+        
+        f_where_SQL = ''''''
+        if f_min_area or f_max_area:
+            if f_min_area == '':
+                f_where_SQL = '''
+                    WHERE way_area <= %(f_max_area)s
+                '''
+            elif f_max_area == '':
+                f_where_SQL = '''
+                    WHERE way_area >= %(f_min_area)s
+                '''
+            elif f_min_area <= f_max_area:
+                f_where_SQL = '''
+                    WHERE way_area >= %(f_min_area)s AND way_area <= %(f_max_area)s
+                '''
+        
+        if f_min_distance or f_max_distance:
+            if f_min_distance == '':
+                sqlForRegions += '''
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(way, 3857)), %(f_max_distance)s) AS forest_buffer_way
+                        FROM planet_osm_polygon
+                    '''
+                sqlForRegions += f_where_SQL
+                sqlForRegions += '''
+                    )
+                '''
+            elif f_max_distance == '':
+                f_max_distance = f_min_distance + 100000000
+                sqlForRegions += '''
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(way, 3857)), %(f_max_distance)s) AS forest_buffer_way
+                        FROM planet_osm_polygon
+                    '''
+                sqlForRegions += f_where_SQL
+                sqlForRegions += '''
+                    )
+                '''
+            elif f_min_distance <= f_max_distance:
+                sqlForRegions += '''
+                    INNER JOIN (
+                        SELECT ST_Buffer(ST_Union(ST_Transform(way, 3857)), %(f_max_distance)s) AS forest_buffer_way
+                        FROM planet_osm_polygon
+                    '''
+                sqlForRegions += f_where_SQL
+                sqlForRegions += '''
+                    )
+                '''
+            sqlForRegions += '''
+                AS forest_buffer
+                ON ST_Intersects(pl_plot3857.geom, forest_buffer.forest_buffer_way)
+            '''
+
+        
+        sqlForRegions += ''';'''
+        with connection.cursor() as cursor:
+            cursor.execute(sqlForRegions, {
+                'l_min_area': l_min_area,
+                'l_max_area': l_max_area,
+                'r_min_distance': r_min_distance,
+                'r_max_distance': r_max_distance,
+                'r_min_length': r_min_length,
+                'r_max_length': r_max_length,
+                'r_min_width': r_min_width,
+                'r_max_width': r_max_width,
+                'i_min_distance': i_min_distance,
+                'i_max_distance': i_max_distance,
+                'i_min_area': i_min_area,
+                'i_max_area': i_max_area,
+                'f_min_distance': f_min_distance,
+                'f_max_distance': f_max_distance,
+                'f_min_area': f_min_area,
+                'f_max_area': f_max_area,
+            })
+            overlapping_regions = cursor.fetchall()
+        print('overlapping_regions')
+        return Response(overlapping_regions)
+
+        
+
+
         # item_to_search = []
         # for item in data['toSearch']:
         #     if data['toSearch'][item]:
@@ -68,26 +360,26 @@ def complex_Search(request):
 
         # Get variables each
 
-        r_option = data['options']['river']
+        # r_option = data['options']['river']
 
-        r_distance_isset = r_option['distance']['isSet']
-        r_distance_mindistance_isset = r_option['distance']['minDistance']['isSet']
-        r_distance_mindistance = r_option['distance']['minDistance']['value']
-        r_distance_maxdistance = r_option['distance']['maxDistance']['value']
+        # r_distance_isset = r_option['distance']['isSet']
+        # r_distance_mindistance_isset = r_option['distance']['minDistance']['isSet']
+        # r_distance_mindistance = r_option['distance']['minDistance']['value']
+        # r_distance_maxdistance = r_option['distance']['maxDistance']['value']
 
-        r_length_isset = r_option['length']['isSet']
-        r_length_minlength = r_option['length']['minLength']['value']
-        r_length_maxlength_isset = r_option['length']['maxLength']['isSet']
-        r_length_maxlength = r_option['length']['maxLength']['value']
+        # r_length_isset = r_option['length']['isSet']
+        # r_length_minlength = r_option['length']['minLength']['value']
+        # r_length_maxlength_isset = r_option['length']['maxLength']['isSet']
+        # r_length_maxlength = r_option['length']['maxLength']['value']
 
-        r_width_isset = r_option['width']['isSet']
-        r_width_minwidth = r_option['width']['minWidth']['value']
-        r_width_maxwidth_isset = r_option['width']['maxWidth']['isSet']
-        r_width_maxwidth = r_option['width']['maxWidth']['value']
+        # r_width_isset = r_option['width']['isSet']
+        # r_width_minwidth = r_option['width']['minWidth']['value']
+        # r_width_maxwidth_isset = r_option['width']['maxWidth']['isSet']
+        # r_width_maxwidth = r_option['width']['maxWidth']['value']
 
-        r_name_isset = r_option['name']['isSet']
-        r_name_method = r_option['name']['method']['value']
-        r_name_reference = r_option['name']['reference']['value']
+        # r_name_isset = r_option['name']['isSet']
+        # r_name_method = r_option['name']['method']['value']
+        # r_name_reference = r_option['name']['reference']['value']
 
         # r_length = data.get('R_Length')
         # r_width = data.get('R_Width')
@@ -131,34 +423,32 @@ def complex_Search(request):
         #         ON ST_Intersects(pl_plot3857.geom, water_buffer.buffer_geom);
         #     '''
         # l_area = 10
-        sqlForRegions = '''
-            SELECT
-                ST_AsGeoJSON(ST_Union(ST_Transform(pl_plot3857.geom, 3857)))
-            FROM
-                pl_plot3857
-            INNER JOIN (
-                SELECT ST_Buffer(
-                    ST_Union(
-                        ST_Transform(ST_Force2D(geom), 3857)
-                    ),
-                    %(r_distance_maxdistance)s
-                ) AS buffer_geom
-                FROM (
-                    SELECT geom
-                    FROM pl_river3857
-                    WHERE dlug >=  %(r_length_minlength)s AND r_width >= %(r_width_minwidth)s
-                ) AS water_geom
-            ) AS water_buffer
-            ON ST_Intersects(pl_plot3857.geom, water_buffer.buffer_geom);
-        '''
+        # sqlForRegions = '''
+        #     SELECT
+        #         ST_AsGeoJSON(ST_Union(ST_Transform(pl_plot3857.geom, 3857)))
+        #     FROM
+        #         pl_plot3857
+        #     INNER JOIN (
+        #         SELECT ST_Buffer(
+        #             ST_Union(
+        #                 ST_Transform(ST_Force2D(geom), 3857)
+        #             ),
+        #             %(r_distance_maxdistance)s
+        #         ) AS buffer_geom
+        #         FROM (
+        #             SELECT geom
+        #             FROM pl_river3857
+        #             WHERE dlug >=  %(r_length_minlength)s AND r_width >= %(r_width_minwidth)s
+        #         ) AS water_geom
+        #     ) AS water_buffer
+        #     ON ST_Intersects(pl_plot3857.geom, water_buffer.buffer_geom);
+        # '''
 
 
-        with connection.cursor() as cursor:
-            cursor.execute(sqlForRegions,{'r_distance_maxdistance': r_distance_maxdistance, 'r_length_minlength': r_length_minlength, 'r_width_minwidth': r_width_minwidth})
-            overlapping_regions = cursor.fetchall()
+        # with connection.cursor() as cursor:
+        #     cursor.execute(sqlForRegions,{'r_distance_maxdistance': r_distance_maxdistance, 'r_length_minlength': r_length_minlength, 'r_width_minwidth': r_width_minwidth})
+        #     overlapping_regions = cursor.fetchall()
 
-        print('overlapping_regions')
-        return Response(overlapping_regions)
     
 # def complex_Search(request):
 #     if request.method == 'GET':
